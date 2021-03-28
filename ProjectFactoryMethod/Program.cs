f2e4497faada8f4cb06ee17e26312ccbff9c3473
Arctra234 <73104534+Arctra234@users.noreply.github.com>
2021-03-28 12:31:56 +0200

    new change project

--- ProjectFactoryMethod/Program.cs
+++ ProjectFactoryMethod/Program.cs
@@ -4,70 +4,85 @@ namespace ProjectFactoryMethod
 {
     class Program
     {
-        //Tworzymy program za pomocą którego robimy broń (metalową i złotą)
+        //Tworzymy program za pomocą którego robimy broń (miecz i topór) róznych rodzajów
         static void Main(string[] args)
         {
-            Smith smith = new GoldSmith("John Smith");
+            Smith smith = new ConcreteSmith("John Smith");
             Console.WriteLine(smith.Name);
-            Weapon house2 = smith.Create();
-
-            smith = new MetalSmith("Sam Smith");
-            Console.WriteLine(smith.Name);
-            Weapon house = smith.Create();
+            ISmith axe = smith.GetWeapon("Axe");
+            axe.Type("Golden");
+            ISmith sword = smith.GetWeapon("Sword");
+            sword.Type("Wodden");
+            
+            Smith smith1 = new ConcreteSmith("Sam Smith");
+            Console.WriteLine(smith1.Name);
+            ISmith axe1 = smith.GetWeapon("Axe");
+            axe1.Type("Wodden");
+            ISmith sword1 = smith.GetWeapon("Sword");
+            sword1.Type("Metal");
 
             Console.ReadLine();
         }
     }
+    //interface kowala dla wybora typu broni
+    interface ISmith
+    {
+        void Type(string tweapon);
+    }
+
     //klasa abstraktna kowala
     abstract class Smith
     {
+        public abstract ISmith GetWeapon(string Weapon);
+
         public string Name { get; set; }
 
-        public Smith (string n)
+        public Smith(string n)
         {
             Name = n;
         }
-        //Realizacja Factory method
-        abstract public Weapon Create();
-    }
-    //Robimy złotą broń
-    class GoldSmith : Smith
+
+
+    } 
+    
+    
+    //klassa tworzenia broni
+    class ConcreteSmith : Smith
     {
-        public GoldSmith(string n) : base(n)
+
+        public ConcreteSmith (string n) : base(n)
         { }
 
-        public override Weapon Create()
+        public override ISmith GetWeapon(string Weapon)
         {
-            return new GoldWeapon();
-        }
-    }
-    //Robimy metalową broń
-    class MetalSmith : Smith
-    {
-        public MetalSmith(string n) : base(n)
-        { }
-
-        public override Weapon Create()
-        {
-            return new MetalWeapon();
+            switch(Weapon)
+            {
+                case "Axe":
+                    return new Axe();
+                case "Sword":
+                    return new Sword();
+                default:
+                    throw new ApplicationException(string.Format("Weapon'{0}'cannot be created", Weapon));
+            }
+            
         }
     }
     
-    abstract class Weapon
-    { }
+    
 
-    class GoldWeapon : Weapon
+    class Axe : ISmith
     {
-        public GoldWeapon()
-        {
-            Console.WriteLine("Golden weapon ready");
-        }
+         public void Type(string tweapon)
+            {
+                Console.WriteLine("Type the Axe : " + tweapon );
+            }
+        
     }
-    class MetalWeapon : Weapon
+    class Sword : ISmith
     {
-        public MetalWeapon()
+        public void Type(string tweapon)
         {
-            Console.WriteLine("Metal weapon ready");
+            Console.WriteLine("Type the Sword : " + tweapon);
         }
     }
 }
