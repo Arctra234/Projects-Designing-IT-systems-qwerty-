b9861c180be26b4e40deca389e430863706821c0
Arctra234 <73104534+Arctra234@users.noreply.github.com>
2021-04-21 22:36:32 +0200

    new project mvc
    a new project that uses the MVC pattern

---
+++ ModelViewControllerProject/ModelViewControllerProject/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelViewControllerProject.Models;
+
+namespace ModelViewControllerProject.Controllers
+{
+    public class HomeController : Controller
+    {
+       public ViewResult Index()
+        {
+            
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guest)
+        {
+            if (ModelState.IsValid)
+                
+                return View("Thanks", guest);
+            else
+                // Wykryto błąd sprawdzania wiarygodności
+                return View();
+        }
+
+
+    }
+}
---
+++ ModelViewControllerProject/ModelViewControllerProject/Models/GuestResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelViewControllerProject.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Proszę wpisać swoje imię")]
+
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Proszę wpisać swój adres e-mail")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Podałeś nieprawidłowy adres e-mail")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Proszę podać swój telefon")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Wskaż, czy będziesz uczestniczyć w imprezę")]
+        public bool? WillAttend { get; set; }
+    }
+}
